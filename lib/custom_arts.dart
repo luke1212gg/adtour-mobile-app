d5c11d293a5b82c1da0a062f01aa5600860acd65
luke1212gg <103636595+luke1212gg@users.noreply.github.com>
2022-11-23 03:20:20 +0800

    final

---
+++ lib/custom_arts.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color.fromRGBO(33, 118, 198, 1);
+Color bgColor = Color.fromARGB(0, 255, 255, 255);
+Color accentColor = Colors.grey;
+Color categoryTextBGColor = Color.fromARGB(200, 255, 255, 255);
+Color buttonBackgroundColor = primaryColor;
+Color destinationCardTextBGColor = Colors.blue;
+Color destinationCardTextColor = Colors.white;
+Color whatIsThisAllAboutTextColor = Colors.blue;
+Color profileNameColor = Colors.blue;
+Color commentTimestampColor = Colors.black26;
+Color positiveFeedbackColor = Colors.green;
+Color negativeFeedbackColor = Colors.red;
+Color categoryInfoTypeColor = Colors.purple;
+Color destinationInfoFloatingButtonBGColor = Colors.blue;
+Color destinationInfoFloatingButtonIconColor = Colors.white;
